32f049cc49f05b666359ba7e58f19398132937bb
Ricardo <ricardoyassuoikematsu@gmail.com>
2024-09-26 17:08:12 -0300

    primeiro commit

---
+++ microservices-kotlin/src/test/kotlin/br/com/ikematsu/startupTests.kt
@@ -0,0 +1,13 @@
+package br.com.ikematsu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class startupTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
